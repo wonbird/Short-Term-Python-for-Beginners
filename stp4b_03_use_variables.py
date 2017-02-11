b5fc5046a8dded34a4a6daea26fa1c215844f763
wonbird <wonbird@gmail.com>
2017-02-11 12:17:33 +0900

    Add stp4b_03_use_variables.py

---
+++ stp4b_03_use_variables.py
@@ -0,0 +1,17 @@
+>>> spam = 100
+>>> spam
+100
+>>> egg = 90
+>>> egg
+90
+>>> spam + egg
+190
+>>> spam = spam * 2
+>>> spam
+200
+>>> spam = 'Hello'
+>>> spam
+'Hello'
+>>> spam = 'Bye'
+>>> spam
+'Bye'
