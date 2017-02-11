256ef22e9a23614b6a596ebd554f4f7c18f9a491
wonbird <wonbird@gmail.com>
2017-02-11 13:30:21 +0900

    Add stp4b_09_use_continue_break.py

---
+++ stp4b_09_use_continue_break.py
@@ -0,0 +1,16 @@
+#continue & break문
+while True:
+    print('누구십니까?')
+    your_name = input()
+    if your_name == '도깨비':
+        print('안녕하세요. 도깨비님!')
+    elif your_name == '저승사자':
+        print('저승사자님, 또 야근이시군요!')
+    elif your_name == 'pass':
+        continue
+        print('이 문장은 결코 출력되지 않을 겁니다.')
+    elif your_name == 'exit':
+        break
+    else:
+        print(your_name + '..., 뉘신지?')
+    print()
