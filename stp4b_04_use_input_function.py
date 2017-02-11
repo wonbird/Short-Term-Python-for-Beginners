1e18a2f66bb4b5b150411031a5f1292908a32a2b
wonbird <wonbird@gmail.com>
2017-02-11 20:46:26 +0900

    update

--- stp4b_04_use_input_function.py
+++ stp4b_04_use_input_function.py
@@ -1,8 +1,8 @@
 # 인사를 하고 이름을 묻는 프로그램
 print('안녕하세요! 저는 파이썬입니다.')
 print('당신은 누구십니까?')
-your_name=input()
+your_name = input()
 print(your_name+'님, 만나서 반갑습니다.')
 print('올해 몇 살이세요?')
-your_age=input()
-print('내년이면 '+str(int(your_age)+1)+'살이 되는군요.')
+your_age = input()
+print('내년이면 ' + str(int(your_age) + 1) + '살이 되는군요.')
