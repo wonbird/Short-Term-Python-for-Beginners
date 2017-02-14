055a3153b7d101cf607670063168b6b25b0439b5
wonbird <wonbird@gmail.com>
2017-02-14 22:57:42 +0900

    Add stp4b_13_define_class.py

---
+++ stp4b_13_define_class.py
@@ -0,0 +1,25 @@
+class Night:
+    level = 1
+    life = 10
+
+    def __init__(self, name):
+        self.name = name
+
+    def attack(self):
+        print('Yap!')
+        self.life -= 1
+
+    def check_life(self):
+        if self.life <= 0:
+            print('나 ' + self.name + ', 너와 함께 한 시간 모두 눈부셨다. 이 불멸의 저주를 끝내고 무로 돌아간다.')
+        else:
+            print('나 ' + self.name + ', 생명이 ' + str(self.life) + ' 남았다. 죽음이 나에게로 걸어온다.')
+
+night1 = Night('김신')
+night2 = Night('무신')
+
+night1.attack()
+night2.attack()
+night2.attack()
+night1.check_life()
+night2.check_life()
