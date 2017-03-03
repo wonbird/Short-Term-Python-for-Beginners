9a05cfc27c2bce8d649fc64f14213df52b74cad3
wonbird <wonbird@gmail.com>
2017-03-04 07:18:30 +0900

    update

--- stp4b_07_use_while.py
+++ stp4b_07_use_while.py
@@ -1,11 +1,11 @@
 # while문
 while True:
     print('당신은 누구십니까?')
-    your_name=input()
-    if your_name=='도깨비':
+    your_name = input()
+    if your_name == '도깨비':
         print('안녕하세요. 도깨비님!')
-    elif your_name=='저승사자':
+    elif your_name == '저승사자':
         print('저승사자님, 또 야근이시군요!')
     else:
-        print(your_name+'..., 뉘신지?')
+        print(your_name + '..., 뉘신지?')
     print()
